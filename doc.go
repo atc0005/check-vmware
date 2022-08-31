3413803d0838c05ffc581b9593387a1572472f25
Adam Chalkley <atc0005@users.noreply.github.com>
2022-08-31 04:54:51 -0500

    Adjust doc comments formatting for project doc.go
    Update list formatting to remove extra newlines

--- doc.go
+++ doc.go
@@ -18,37 +18,21 @@ Nagios plugins for monitoring VMware vSphere environments (standalone ESXi
 hosts or vCenter instances).
 
   - VMware Tools
-
   - Virtual CPU allocations
-
   - Virtual hardware versions: homogenous, outdated-by threshold range, minimum required and default is minimum required checks
-
   - Host/Datastore/Virtual Machine pairings (using provided Custom Attribute)
-
   - Datastore usage
-
   - Datastore performance
-
   - Snapshots age
-
   - Snapshots count
-
   - Snapshots size
-
   - Resource Pools: Memory usage
-
   - Host Memory usage
-
   - Host CPU usage
-
   - Virtual Machine (power cycle) uptime
-
   - Virtual Machine disk consolidation status (with optional forced refresh of Virtual Machine state data)
-
   - Virtual Machine interactive question status
-
   - Triggered Alarms in one or more datacenters
-
   - Last Backup date for VMs (via specified custom attribute)
 
 # USAGE
