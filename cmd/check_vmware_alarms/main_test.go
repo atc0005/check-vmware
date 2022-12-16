ffa1c6f850de295ba4367ec501bee005d58d2ac3
Adam Chalkley <atc0005@users.noreply.github.com>
2022-12-16 05:24:29 -0600

    Drop plugin runtime tracking, update library usage
    - Allow the new nagios package functionality to handle tracking and
      emitting the `time` metric automatically at plugin completion
    - Add test from the atc0005/go-nagios project to assert that the
      inherited/automatic `time` metric provided by the nagios package is
      emitted at plugin completion
      - since plugins in this project no longer explicitly track this
        performance data metric this test asserts that the nagios package
        continues to provide it
    - Update nagios library usage to reflect dep changes
      - use nagios.NewPlugin constructor in place of manual initialization
      - swap retired ExitState type for new Plugin type
    
    refs GH-747

--- cmd/check_vmware_alarms/main_test.go
+++ cmd/check_vmware_alarms/main_test.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/atc0005/check-vmware/internal/config"
 	"github.com/atc0005/check-vmware/internal/vsphere"
+	"github.com/atc0005/go-nagios"
 	"github.com/google/go-cmp/cmp"
 	"github.com/vmware/govmomi/vim25/types"
 )
@@ -1346,4 +1347,44 @@ func TestFilters(t *testing.T) {
 
 	t.Logf("Finished processing %d test cases", len(tests))
 
+}
+
+// TestEmptyClientPerfDataAndConstructedPluginProducesDefaultTimeMetric
+// asserts that omitted performance data from client code produces a default
+// time metric when using the Plugin constructor.
+func TestEmptyClientPerfDataAndConstructedPluginProducesDefaultTimeMetric(t *testing.T) {
+	t.Parallel()
+
+	// Setup Plugin type the same way that client code using the
+	// constructor would.
+	plugin := nagios.NewPlugin()
+
+	// Performance Data metrics are not emitted if we do not supply a
+	// ServiceOutput value.
+	plugin.ServiceOutput = "TacoTuesday"
+
+	var outputBuffer strings.Builder
+
+	plugin.SetOutputTarget(&outputBuffer)
+
+	// os.Exit calls break tests
+	plugin.SkipOSExit()
+
+	// Process exit state, emit output to our output buffer.
+	plugin.ReturnCheckResults()
+
+	want := fmt.Sprintf(
+		"%s | %s",
+		plugin.ServiceOutput,
+		"'time'=",
+	)
+
+	got := outputBuffer.String()
+
+	if !strings.Contains(got, want) {
+		t.Errorf("ERROR: Plugin output does not contain the expected time metric")
+		t.Errorf("\nwant %q\ngot %q", want, got)
+	} else {
+		t.Logf("OK: Emitted performance data contains the expected time metric.")
+	}
 }
